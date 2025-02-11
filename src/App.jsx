65c2057d727b4f3988f432911debed1da426df29
saqlainkoser <saqlainkoser@gmail.com>
2025-02-11 10:27:26 +0530

    G5 class code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 // import './App.css'
 
+import Navbar from "./components/Navbar"
+
 const App = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar/>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+      <section className='w-full nav-height bg-black relative'>
+        <div className='h-5/6 w-full flex-center flex-col'>
+            <p id="hero" className='hero-title'>iPhone 15 Pro</p>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Hero
